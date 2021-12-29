c80f6fdbacafdebd4f52ceee7411d10d4a113c85
Yajat Vishwak <yajat472001@gmail.com>
2021-12-29 17:32:51 +0530

    bro

--- backend/server/models.py
+++ backend/server/models.py
@@ -3,23 +3,40 @@ from server import db
 class Student(db.Model):
     sID = db.Column(db.Integer, primary_key = True)
     usn = db.Column(db.String(20), unique = True, nullable=False)
-    deviceID = db.Column(db.String(100), unique = True, nullable=False)
+    name = db.Column(db.String(50), unique = True, nullable=False)
+    # deviceID = db.Column(db.String(100), unique = True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    classID = db.Column(db.String(60), nullable=False)
-    attendance = db.relationship("Attendance",backref="student")
-    def __repr__(self) -> str:
-        return f"User('{self.usn}','{self.deviceID}','{self.classID}')"
+    classCode = db.Column(db.String(60), nullable=False)
+    subjectCodes = db.Column(db.String(60), nullable=False)
+    #attendance = db.relationship("Attendance",backref="student")
+
+class Teacher(db.Model):
+    tID = db.Column(db.Integer, primary_key = True)
+    usn = db.Column(db.String(20), unique = True, nullable=False)
+    name = db.Column(db.String(50), unique = True, nullable=False)
+    # deviceID = db.Column(db.String(100), unique = True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
 class Attendance(db.Model):
     aID = db.Column(db.Integer, primary_key= True)
-    sID = db.Column(db.String(10), db.ForeignKey("student.sID"))
+    sID = db.Column(db.String(10))
     sessionID = db.Column(db.String(20),unique = True, nullable= False)
-    status = db.Column(db.String(20) ,default="pending")
-    xcord  = db.Column(db.Integer)
-    ycord  = db.Column(db.Integer)
-    courseCode = db.Column(db.String(100))
+    # xcord  = db.Column(db.Integer)
+    # ycord  = db.Column(db.Integer)
 
-class ClassCourse(db.Model):
-    classID = db.Column(db.String(60), primary_key = True)
-    subName = db.Column(db.String(100))
-    courseCode = db.Column(db.String(100), unique=True)
+class Session(db.Model):
+
+    tid = db.Column(db.Integer )
+    sessionID = db.Column(db.Integer, primary_key= True)
+    classCode = db.Column(db.String(10))
+    subjectCode = db.Column(db.String(20),unique = True, nullable= False)
+    timeInterval = db.Column(db.String(100))
+    date = db.Column(db.String(100))
+    # status = db.Column(db.String(20) ,default="pending")
+    # xcord  = db.Column(db.Integer)
+    # ycord  = db.Column(db.Integer)
+
+# class ClassCourse(db.Model):
+#     classID = db.Column(db.String(60), primary_key = True)
+#     subName = db.Column(db.String(100))
+#     courseCode = db.Column(db.String(100), unique=True)
--- backend/server/routes.py
+++ backend/server/routes.py
@@ -1,7 +1,7 @@
 from server import app
 from flask.globals import request
 from flask.json import jsonify
-from server.models import Student 
+from server.models import Student, Teacher 
 
 
 @app.route("/" , methods= ["GET"])
@@ -9,8 +9,8 @@ def home():
     d = {"message": "Attendify Server Online"}
     return jsonify(d)
 
-@app.route("/login" , methods= ["POST"])
-def login():
+@app.route("/loginstudent" , methods= ["POST"])
+def loginstudent():
     request_data = request.get_json()
     usn = request_data["usn"]
     password =  request_data["password"]
@@ -20,6 +20,33 @@ def login():
     if user == None:
         return jsonify({"message": "auth unsuccessful"})
     if user.password == password:
-        return jsonify({"message": "auth successful","deviceID": user.deviceID})
+        return jsonify({"message": "auth successful"})
     else:
         return jsonify({"message": "auth unsuccessful"})
+
+
+@app.route("/loginteacher" , methods= ["POST"])
+def loginteacher():
+    request_data = request.get_json()
+    usn = request_data["usn"]
+    password =  request_data["password"]
+    user = Teacher.query.filter_by(usn=usn).first()
+    print(user)
+   # return jsonify(True)
+    if user == None:
+        return jsonify({"message": "auth unsuccessful"})
+    if user.password == password:
+        return jsonify({"message": "auth successful"})
+    else:
+        return jsonify({"message": "auth unsuccessful"})
+
+
+@app.route("/createSession" , methods= ["POST"])
+def createSession():
+    request_data = request.get_json()
+    subjectCode = request_data["subjectCode"]
+    classCode =  request_data["classCode"]
+    timing =  request_data["timing"]
+    date =  request_data["date"]
+    
+    
