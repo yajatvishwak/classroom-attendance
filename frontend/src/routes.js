1ebb09e952e8d442ac4a71b7d150d173e8e9716d
Yajat Vishwak <yajat472001@gmail.com>
2021-12-29 09:11:28 +0530

    teacher

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,8 +4,10 @@ import NotFound from "./views/NotFound.vue";
 import StudentLogin from "@/views/StudentLogin.vue";
 import StudentSignup from "@/views/StudentSignup.vue";
 import StudentDashboard from "@/views/StudentDashboard.vue";
+import TeacherDashboard from "@/views/TeacherDashboard.vue";
 export const routes = [
   { path: "/studentlogin", component: StudentLogin },
   { path: "/studentsignup", component: StudentSignup },
   { path: "/studentdashboard", component: StudentDashboard },
+  { path: "/teacherdashboard", component: TeacherDashboard },
 ];
